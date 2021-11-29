21632904085ad7872b952f3f32188083245446b1
shibuiwilliam <shibuiyusuke@gmail.com>
2021-11-29 20:55:40 +0900

    update

--- chapter3_4_animal_app/backend/src/middleware/database.py
+++ chapter3_4_animal_app/backend/src/middleware/database.py
@@ -4,17 +4,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-postgres_username = os.getenv("POSTGRES_USER")
-postgres_password = os.getenv("POSTGRES_PASSWORD")
-postgres_port = int(os.getenv("POSTGRES_PORT", 5432))
-postgres_db = os.getenv("POSTGRES_DB")
-postgres_server = os.getenv("POSTGRES_SERVER")
+__postgres_username = os.getenv("POSTGRES_USER")
+__postgres_password = os.getenv("POSTGRES_PASSWORD")
+__postgres_port = int(os.getenv("POSTGRES_PORT", 5432))
+__postgres_db = os.getenv("POSTGRES_DB")
+__postgres_server = os.getenv("POSTGRES_SERVER")
 
-sql_alchemy_database_url = f"postgresql://{postgres_username}:{postgres_password}@{postgres_server}:{postgres_port}/{postgres_db}?client_encoding=utf8"
+__sql_alchemy_database_url = f"postgresql://{__postgres_username}:{__postgres_password}@{__postgres_server}:{__postgres_port}/{__postgres_db}?client_encoding=utf8"
 
 
 engine = create_engine(
-    sql_alchemy_database_url,
+    __sql_alchemy_database_url,
     encoding="utf-8",
     pool_recycle=3600,
     echo=False,
--- chapter3_4_animal_app/backend/src/usecase/abstract_usecase.py
+++ chapter3_4_animal_app/backend/src/usecase/abstract_usecase.py
@@ -1,9 +1,5 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 from logging import getLogger
-from typing import List, Optional
-
-from sqlalchemy.orm import Session
-from src.schema.abstract_schema import AbstractCreate, AbstractModel, AbstractQuery
 
 logger = getLogger(__name__)
 
