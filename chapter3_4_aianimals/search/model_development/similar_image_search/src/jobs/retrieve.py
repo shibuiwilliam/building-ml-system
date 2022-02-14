09f4a9c034a09825547d7ee3fb805cfc3d896b48
shibuiwilliam <shibuiyusuke@gmail.com>
2022-02-14 20:59:16 +0900

    update

--- chapter3_4_aianimals/search/model_development/similar_image_search/src/jobs/retrieve.py
+++ chapter3_4_aianimals/search/model_development/similar_image_search/src/jobs/retrieve.py
@@ -5,6 +5,8 @@ from typing import List
 
 import httpx
 from PIL import Image
+from src.dataset.data_manager import AbstractDBClient, AnimalRepository
+from src.dataset.schema import Animal
 
 
 async def download_file(
@@ -55,3 +57,35 @@ def download_dataset(
         )
     )
     return destination_paths
+
+
+def retrieve_animals(
+    db_client: AbstractDBClient,
+) -> RawData:
+    access_log_repository = AccessLogRepository(db_client=db_client)
+    records = access_log_repository.select_all()
+    data = []
+    target = []
+    for record in records:
+        d = dict(
+            animal_id=record.animal_id,
+            query_phrases=".".join(sorted(record.query_phrases)),
+            query_animal_category_id=record.query_animal_category_id,
+            query_animal_subcategory_id=record.query_animal_subcategory_id,
+            likes=record.likes,
+            animal_category_id=record.animal_category_id,
+            animal_subcategory_id=record.animal_subcategory_id,
+            name=record.name,
+            description=record.description,
+        )
+        data.append(d)
+        if record.action == Action.SELECT:
+            target.append([1])
+        elif record.action == Action.SEE_LONG:
+            target.append([3])
+        elif record.action == Action.LIKE:
+            target.append([4])
+    return RawData(
+        data=data,
+        target=target,
+    )
