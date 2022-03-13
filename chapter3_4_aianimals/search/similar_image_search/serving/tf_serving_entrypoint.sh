d44345aafb395060ba711fde30d37ce90f3dcd64
shibuiwilliam <shibuiyusuke@gmail.com>
2022-03-13 18:30:48 +0900

    update

--- chapter3_4_aianimals/search/similar_image_search/serving/tf_serving_entrypoint.sh
+++ chapter3_4_aianimals/search/similar_image_search/serving/tf_serving_entrypoint.sh
@@ -5,7 +5,7 @@ set -eu
 PORT=${PORT:-8500}
 REST_API_PORT=${REST_API_PORT:-8501}
 MODEL_NAME=${MODEL_NAME:-"similar_image_search"}
-MODEL_BASE_PATH=${MODEL_BASE_PATH:-"/similar_image_search/saved_model/${MODEL_NAME}"}
+MODEL_BASE_PATH=${MODEL_BASE_PATH:-"/models/saved_model/${MODEL_NAME}"}
 
 tensorflow_model_server \
     --port=${PORT} \
