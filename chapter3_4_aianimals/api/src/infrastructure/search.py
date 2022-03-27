219dfd148e702cb918ddd6b342016ea9c1d4d01b
shibuiwilliam <shibuiyusuke@gmail.com>
2022-03-27 14:52:42 +0900

    update

--- chapter3_4_aianimals/api/src/infrastructure/search.py
+++ chapter3_4_aianimals/api/src/infrastructure/search.py
@@ -183,6 +183,7 @@ class ElasticsearchClient(AbstractSearch):
             results=[],
             offset=from_ + min(size, searched["hits"]["total"]["value"]),
         )
+        logger.info(f"DDDDDDDDDDDDDDDDDDDDDDDDD: {results}")
         for r in searched["hits"]["hits"]:
             results.results.append(
                 AnimalSearchResult(
@@ -200,4 +201,5 @@ class ElasticsearchClient(AbstractSearch):
                     created_at=r["_source"]["created_at"],
                 ),
             )
+        logger.info(f"EEEEEEEEEEEEEEEEEEEEEEEE: {results}")
         return results
