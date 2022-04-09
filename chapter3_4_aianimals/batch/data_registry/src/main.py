c55b17823721dccc2237f1cd8ccf01ad4d6c1ef5
shibuiwilliam <shibuiyusuke@gmail.com>
2022-04-09 19:56:19 +0900

    update

--- chapter3_4_aianimals/batch/data_registry/src/main.py
+++ chapter3_4_aianimals/batch/data_registry/src/main.py
@@ -1,5 +1,6 @@
 from dependency_injector.wiring import Provide, inject
 from src.configurations import Configurations
+from src.infrastructure.messaging import RabbitmqMessaging
 from src.job.animal_to_search_job import AnimalToSearchJob
 from src.job.initialization_job import InitializationJob
 from src.job.jobs import JOBS
@@ -8,9 +9,16 @@ from src.registry.container import Container
 
 @inject
 def main(
+    messaging: RabbitmqMessaging = Provide[Container.infrastructures.messaging],
     initialization_job: InitializationJob = Provide[Container.jobs.initialization_job],
     animal_search_job: AnimalToSearchJob = Provide[Container.jobs.animal_search_job],
 ):
+    messaging.init_channel()
+    for q in Configurations.animal_violation_queues:
+        messaging.create_queue(queue_name=q)
+    messaging.create_queue(queue_name=Configurations.animal_registry_queue)
+    messaging.create_queue(queue_name=Configurations.animal_feature_registry_queue)
+
     if Configurations.job == JOBS.ANIMAL_TO_SEARCH_JOB.value.name:
         animal_search_job.run()
     elif Configurations.job == JOBS.INITIALIZATION_JOG.value.name:
