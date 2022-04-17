1e8f6151f6a319d3fc20c0925450db4e690f92f0
shibuiwilliam <shibuiyusuke@gmail.com>
2022-04-17 11:26:59 +0900

    update

--- chapter3_4_aianimals/api/src/main.py
+++ chapter3_4_aianimals/api/src/main.py
@@ -5,7 +5,6 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from src.api import access_log, animal, animal_category, health_check, like, metadata, user, violation
 from src.configurations import Configurations
-from src.constants import RUN_ENVIRONMENT
 from src.exceptions.custom_exceptions import APINotAllowedException, DatabaseException, StorageClientException
 
 logger = getLogger(__name__)
@@ -21,11 +20,6 @@ app = FastAPI(
     docs_url=f"{base_prefix}/docs",
     redoc_url=f"{base_prefix}/redoc",
 )
-
-if Configurations.run_environment == RUN_ENVIRONMENT.CLOUD.value:
-    from prometheus_fastapi_instrumentator import Instrumentator
-
-    Instrumentator().instrument(app).expose(app)
 
 
 @app.exception_handler(DatabaseException)
