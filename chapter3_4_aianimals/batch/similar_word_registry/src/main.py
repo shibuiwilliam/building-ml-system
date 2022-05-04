7be263555694cdddcdf56350b8b478671c8a52ac
shibuiwilliam <shibuiyusuke@gmail.com>
2022-05-04 17:18:45 +0900

    update

--- chapter3_4_aianimals/batch/similar_word_registry/src/main.py
+++ chapter3_4_aianimals/batch/similar_word_registry/src/main.py
@@ -1,5 +1,5 @@
 from dependency_injector.wiring import Provide, inject
-from registry.container import Application
+from src.registry.container import Application
 from src.job.similar_word_registration_job import AbstractSimilarWordRegistrationJob
 
 
