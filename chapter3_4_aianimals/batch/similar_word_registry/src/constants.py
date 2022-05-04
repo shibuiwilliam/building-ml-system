564e72226db3fb09cfef2e71b4a6e4687854df2b
shibuiwilliam <shibuiyusuke@gmail.com>
2022-05-04 17:21:25 +0900

    update

--- chapter3_4_aianimals/batch/similar_word_registry/src/constants.py
+++ chapter3_4_aianimals/batch/similar_word_registry/src/constants.py
@@ -15,6 +15,16 @@ class RUN_ENVIRONMENT(Enum):
         return [v.value for v in RUN_ENVIRONMENT.__members__.values()]
 
 
+def constant(f):
+    def fset(self, value):
+        raise TypeError
+
+    def fget(self):
+        return f()
+
+    return property(fget, fset)
+
+
 class _CONSTANTS(object):
     @constant
     def SPLITTER() -> str:
