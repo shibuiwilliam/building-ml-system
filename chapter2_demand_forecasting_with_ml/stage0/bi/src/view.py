a2ddfd8edcc6a9b8738968ef2604866b32781542
shibuiwilliam <shibuiyusuke@gmail.com>
2022-05-22 14:53:16 +0900

    update

--- chapter2_demand_forecasting_with_ml/stage0/bi/src/view.py
+++ chapter2_demand_forecasting_with_ml/stage0/bi/src/view.py
@@ -229,6 +229,9 @@ def show_weekly_item_sales_evaluation(
         not_aggregated = "store"
     for li in loop_in:
         _df = df[df[aggregate_by] == li].reset_index(drop=True).sort_values(["year", "month", "week_of_year", sort_by])
+        absolute_min = min(_df.diff.min() - 50, -100)
+        absolute_max = max(max(_df.sales.max(), _df.prediction.max()) + 100, 100)
+
         with st.expander(
             label=f"{aggregate_by} {li}",
             expanded=True,
@@ -274,7 +277,7 @@ def show_weekly_item_sales_evaluation(
             )
             fig.update_yaxes(
                 title_text="numeric",
-                range=[-100, 1000],
+                range=[absolute_min, absolute_max],
                 secondary_y=False,
             )
             fig.update_yaxes(
