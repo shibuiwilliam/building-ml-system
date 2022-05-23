56531c75bedaac3af2cf0ed2a2ef7dcf81b73a77
shibuiwilliam <shibuiyusuke@gmail.com>
2022-05-23 21:39:34 +0900

    update

--- chapter2_demand_forecasting_with_ml/stage1/infrastructure/port_forward.sh
+++ chapter2_demand_forecasting_with_ml/stage1/infrastructure/port_forward.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+kubectl -n beverage-sales-forecasting port-forward service/bi 8501:8501 &
 kubectl -n mlflow port-forward service/mlflow 5000:5000 &
 kubectl -n argo port-forward service/argo-server 2746:2746 &
